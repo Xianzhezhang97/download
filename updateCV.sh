ddee8bb4e33ba9e4a68d1280d2d9837e3ae5b782
Xianzhezhang97 <scott5443003@gmail.com>
2024-10-17 17:06:46 +1100

    Update CV | Scott Update | Thu Oct 17 17:06:46 AEDT 2024 | xianzhes-MacBook-Pro-9.local

---
+++ updateCV.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# 指定输入和输出文件
+input_file="CV | Oringinal.pages"
+output_file="CV | Scott Cheung.pdf"
+
+# 检查 Pages 文件是否存在
+if [ ! -f "$input_file" ]; then
+  echo "输入文件不存在: $input_file"
+  exit 1
+fi
+
+# 使用 AppleScript 将 pages 文件转为 pdf
+osascript <<EOF
+on run
+    tell application "Pages"
+        open (POSIX file "$PWD/$input_file")
+        set outputFile to POSIX file "$PWD/$output_file"
+        export front document to outputFile as PDF
+        close front document
+    end tell
+end run
+EOF
+
+# 检查输出文件是否成功生成
+if [ -f "$output_file" ]; then
+  echo "转换成功: $output_file"
+else
+  echo "转换失败"
+fi
+
+git add .
+git commit -m "Update CV | Scott Update | $(date) | $(hostname)"
+git push
